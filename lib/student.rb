774518ca8501a29bf0a1bd41aed65eafb8b53887
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-03-18 05:09:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,11 +18,11 @@ class Student < InteractiveRecord
     column_names.compact
   end
 
-  def initialize(options={})
-  options.each do |property, value|
-    self.send("#{property}=", value)
+  def initialize(options = {})
+    options.each do |property,value|
+      self.send("#{property}=", value)
+    end
   end
-end
 
   def table_name_for_insert
     self.class.table_name
